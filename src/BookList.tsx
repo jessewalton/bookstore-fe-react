253d716b01e31680e234d6db3d9fa376d72b2236
Jesse Walton <jesse.m.walton@gmail.com>
2020-05-30 09:20:01 -0600

    Format BookList

--- src/BookList.tsx
+++ src/BookList.tsx
@@ -1,38 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 interface Book {
-    id: number,
-    author: string,
-    title: string,
-    url: string,
-  }
-  function BookList() {
-    const [data, setData] = useState<Book[]>([]);
-    
-    useEffect(() => {
-      const fetchData = async () => {
-        const result: any = await axios.get(
-          'http://localhost:8080/books',
-        );
-        console.log(result);
-        
-        setData(result.data.data);
-      };
-  
-      fetchData();
-    }, []);
-  
-    return (
-      <ul>
-        {data && data.map((item: Book) => (
+  id: number;
+  author: string;
+  title: string;
+  url: string;
+}
+function BookList() {
+  const [data, setData] = useState<Book[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result: any = await axios.get("http://localhost:8080/books");
+      setData(result.data.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <ul>
+      {data &&
+        data.map((item: Book) => (
           <li key={item.id}>
-            <a href={item.url}>{item.title} by {item.author}</a>
+            <a href={item.url}>
+              {item.title} by {item.author}
+            </a>
           </li>
         ))}
-      </ul>
-    )
-  
-  }
+    </ul>
+  );
+}
 
-  export default BookList;
+export default BookList;
