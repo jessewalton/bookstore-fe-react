6f2b68be5a898062607c3db037a9090391cfe064
Jesse Walton <jesse.m.walton@gmail.com>
2020-05-30 21:25:59 -0600

    Add echo and book ws

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import Greeting from './Greeting';
-import BookList from './BookList';
-import HackerNewsSearch from './HackerNewsSearch';
-import BookForm from './BookForm';
+import Greeting from "./Greeting";
+import BookList from "./BookList";
+import HackerNewsSearch from "./HackerNewsSearch";
+import BookForm from "./BookForm";
 
 function App() {
+  // create echo ws
+
+  const echoWS: any = useRef(null);
+
+  useEffect(() => {
+    echoWS.current = new WebSocket("wss://echo.websocket.org");
+    echoWS.current.onopen = () => console.log("echo ws opened");
+    echoWS.current.onclose = () => console.log("echo ws closed");
+
+    return () => {
+      echoWS.current.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!echoWS.current) {
+      console.log("no websocket available");
+      return;
+    }
+
+    echoWS.current.onmessage = (e: any) => {
+      // const message = JSON.parse(e.data);
+      console.log("receive:", e.data);
+    };
+  }, []);
+
+  function sendToEcho(data: string) {
+    console.log("send   :", data);
+    echoWS.current.send(data);
+  }
+
+  // end echo ws
+
+  // create book ws
+
+  const bookWS: any = useRef(null);
+
+  useEffect(() => {
+    bookWS.current = new WebSocket("ws://localhost:8080/ws");
+    bookWS.current.onopen = () => console.log("book ws opened");
+    bookWS.current.onclose = () => console.log("book ws closed");
+
+    return () => {
+      bookWS.current.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!bookWS.current) {
+      console.log("no websocket available");
+      return;
+    }
+
+    bookWS.current.onmessage = (e: any) => {
+      // const message = JSON.parse(e.data);
+      console.log("receive:", e.data);
+    };
+  }, []);
+
+  function sendToBackend(data: string) {
+    console.log("send   :", data);
+    bookWS.current.send(data);
+  }
+
+  // end book ws
 
   return (
     <div className="App">
@@ -26,13 +91,21 @@ function App() {
         </a>
       </header>
 
-      <Greeting name="stranger"/>
+      <button
+        onClick={() => {
+          sendToEcho("test to echo");
+          sendToBackend("test to backend ");
+        }}
+      >
+        {"Send Message"}
+      </button>
+
+      <Greeting name="stranger" />
       <HackerNewsSearch />
 
       <BookForm />
-    
-      <BookList />
 
+      <BookList />
     </div>
   );
 }
